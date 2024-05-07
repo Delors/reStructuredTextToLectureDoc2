801aeed39ae623fa104449d4399b4f55931163e2
Michael Eichberg <mail@michael-eichberg.de>
2024-05-07 10:40:32 +0200

    documentation improvements

--- rst2ld.py
+++ rst2ld.py
@@ -4,7 +4,7 @@
 # Copyright: This module has been placed in the public domain.
 
 """
-A minimal front end to the Docutils Publisher, producing HTML slides using LectureDoc 2.
+A minimal front end to the Docutils Publisher, producing HTML documents for LectureDoc2.
 """
 
 from docutils.writers import html5_polyglot
@@ -12,7 +12,7 @@ from docutils.writers import html5_polyglot
 from docutils.core import publish_cmdline, default_description
 from lddocutils.ldwriter import Writer
 
-DESCRIPTION = ('Generates LectureDoc HTML documents from standalone '
+DESCRIPTION = ('Generates LectureDoc2 HTML documents from standalone '
                'reStructuredText sources.  ' + default_description)
 
 publish_cmdline(writer=Writer(), writer_name='html', description=DESCRIPTION)
